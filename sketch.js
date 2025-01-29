5f40be12782337c7cc49a901ebe25cced0dac399
Cole Heausler <146775332+c0lbalt@users.noreply.github.com>
2025-01-28 20:30:44 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+  background("teal");
+}
+
+function draw() {
+  
+    // Set the outline to white
+    stroke(255); // White color
+    strokeWeight(2); // Thickness of the outline
+  
+    // Draw shapes with white outline
+    fill("green"); // Fill color for the shape
+    ellipse(200, 200, 100, 100); // Draw a circle
+  
+    
+  }
+  
+
